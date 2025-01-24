acfc8f7f9c49ab5c770c409479dd46354a213b8a
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-01-24 13:20:05 +0800

    remove connection to mysql in country_codes_to_table.py

--- dataset_files/country_codes_to_table.py
+++ dataset_files/country_codes_to_table.py
@@ -12,15 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, 'database/.env'))
 
-# connect to the database
-mydb = mysql.connector.connect(
-    host = env('DB_HOST'),
-    user = env('DB_USER'),
-    password = env('DB_PASSWORD'),
-    database = env('DB_NAME')
-)
-m = mydb.cursor(prepared=True)
-
 def insert_countries_and_dial_codes(cursor, filename):
     # read the csv and only keep the necessary columns 
     codes = pd.read_csv(os.path.join(BASE_DIR, filename))
