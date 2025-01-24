eaba3625a2dd39a2a53b7947033ca910a201cb82
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-01-24 11:29:12 +0800

    changed connection to db'

--- test.py
+++ test.py
@@ -12,32 +12,19 @@ BASE_DIR = Path(__file__).resolve().parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, 'database/.env'))
 
+config = {
+    'user' : env('DB_USER'),
+    'password' : env('DB_PASSWORD'),
+    'database' : "test_db",
+}
+
 class TestMySQL(unittest.TestCase):
     def setUp(self):
         # connect to mysql database
-        self.connection = mysql.connector.connect (
-            user = env('DB_USER'),
-            password = env('DB_PASSWORD'),
-            database = "test_db",
-        )
+        with mysql.connector.connect(**config) as self.connection:
         
-        # Initialise database for testing
-        self.execute_sql_script('schema.sql')
-        cursor = self.connection.cursor()
-        
-    def test_country_codes(self):
-        # insert into countries and dial_codes
-        cursor = self.connection.cursor()
-        cursor.execute('INSERT INTO countries (code, name) VALUES ("MYS", "Malaysia");')
-        cursor.execute('SELECT name FROM countries WHERE code = "MYS";')
-        country = cursor.fetchone()[0]
-        cursor.close()
-        
-        self.assertEqual('Malaysia', country)
-    
-    def test_antibiotics(self):
-        pass
-        
+            # Initialise database for testing
+            self.execute_sql_script('schema.sql')
     
     def execute_sql_script(self, file_path):
         f = open(file_path, mode='r')
@@ -47,7 +34,19 @@ class TestMySQL(unittest.TestCase):
         for statement in statements:
             # Remove comments
             statement = re.sub('--.*$', '', statement, 0, re.M)
+                # Execute non-empty command
+            cursor.execute(statement)
         cursor.close()
+    
+    def test_country_codes(self):
+    # insert into countries and dial_codes
+        with mysql.connector.connect(**config) as self.connection:
+            cursor = self.connection.cursor()
+            cursor.execute('INSERT INTO countries (code, name) VALUES ("MYS", "Malaysia");')
+            cursor.execute('SELECT name FROM countries WHERE code = "MYS";')
+            country = cursor.fetchone()[0]
+    
+        self.assertEqual('Malaysia', country)
         
         
 if __name__ == '__main__':
