4d879a84d0db0e01fcec63d973b86f82851d0d7f
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-01-28 11:21:22 +0800

    base table component

--- database/frontend/src/components/Home.js
+++ database/frontend/src/components/Home.js
@@ -1,22 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import Table from "./Table";
 
 export default function Home(props) {
+    const [patientList, setPatientList] = useState(null);
+
     useEffect(() => {
         fetch('backend/patients')
         .then(response => response.json())
         .then(result => {
-           console.log(result)
+           setPatientList(result)
         }).catch(error => console.log(error))
     },[]);
+
+    const tableOrder = {
+        'id':'id',
+        'full_name': 'Full name',
+        'email': 'email',
+        'age': 'Age',
+        'phone_number': 'Phone Number',
+    }
 
     return (
         <div>
             <NavBar/>
             <div className="container">
                 <h3>Patient List</h3>
-                <Table/>
+                { patientList == null ? null :
+                <Table tableOrder={tableOrder} tableList={patientList}/>
+                }
             </div>
         </div>
     )
--- database/frontend/src/components/Table.js
+++ database/frontend/src/components/Table.js
@@ -1,8 +1,40 @@
 import React from "react";
 
 export default function Table(props) {
+
+    function TableRow({item}){
+        return(
+            <>
+            {
+                Object.entries(props.tableOrder)
+                .map( ([key, value]) => <td key={key}>{item[key]}</td>)
+            }
+            </>
+        )
+    }
     return (
         <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                    {
+                        Object.entries(props.tableOrder)
+                        .map( ([key, value]) => <th key={key}> {value}</th>)
+                    }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        props.tableList.map((item, key) => {
+                            return (
+                                <tr key={key}>
+                                    <TableRow item={item}/>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
