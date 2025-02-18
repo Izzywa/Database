bc164689db436bb5cea815ca2ccdc51bd804cc2f
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-02-18 09:40:23 +0800

    change to updated amchart module

--- database/frontend/src/components/MapStats.js
+++ database/frontend/src/components/MapStats.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
@@ -6,6 +6,10 @@ import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 am4core.useTheme(am4themes_animated);
 
 export default function MapStats(props) {
+    useEffect(() => {
+        let chart = am4core.create("chartdiv", am4charts.)
+    },[])
+
     return (
         <>
         <div id="chartdiv" style={{ width: "100%", height: "100%"}}>
