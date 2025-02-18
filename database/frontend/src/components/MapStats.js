8898f06f329ceb3b90e63b392d5793b604dee56f
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-02-18 09:43:43 +0800

    installed a newer amcharts

--- database/frontend/src/components/MapStats.js
+++ database/frontend/src/components/MapStats.js
@@ -1,15 +1,12 @@
 import React from "react";
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4charts from '@amcharts/amcharts4/charts';
-import am4themes_animated from '@amcharts/amcharts4/themes/animated';
-
-am4core.useTheme(am4themes_animated);
+import * as am5 from "@amcharts/amcharts5";
+import * as am5map from "@amcharts/amcharts5/map";
 
 export default function MapStats(props) {
     return (
         <>
         <div id="chartdiv" style={{ width: "100%", height: "100%"}}>
-            
+            UPDATE MAP
         </div>
         </>
     )
