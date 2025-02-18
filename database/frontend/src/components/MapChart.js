d8b12336473cb6cbaa7ee9d91148fcad5373e026
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2025-02-18 10:13:52 +0800

    set chart theme

--- database/frontend/src/components/MapChart.js
+++ database/frontend/src/components/MapChart.js
@@ -2,17 +2,27 @@ import React, { useEffect, useLayoutEffect } from "react";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
 export default function MapChart(props) {
     useLayoutEffect(() => {
+        // create root
         let root = am5.Root.new("chartdiv")
+
+        //set themes
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ])
+
+        // create chart
         let chart = root.container.children.push(
             am5map.MapChart.new(root, {
                 projection: am5map.geoNaturalEarth1()
             })
         );
 
-        let poligonSeries = chart.series.push(
+        // create polygon series
+        let polygonSeries = chart.series.push(
             am5map.MapPolygonSeries.new(root, {
                 geoJSON: am5geodata_worldLow
             })
@@ -24,7 +34,7 @@ export default function MapChart(props) {
     
     return (
         <>
-        adding series
+        set themes
         <div id="chartdiv" style={{ width: "100%", height: "100%"}}>
             
         </div>
